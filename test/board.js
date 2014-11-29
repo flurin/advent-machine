a96700742a4d0c59c0cc35137c099be21cd66183
Flurin Egger <flurin@digitpaint.nl>
2014-11-29 18:14:34 +0100

    Add boardtest

---
+++ test/board.js
@@ -0,0 +1,34 @@
+var Color = require("color");
+
+// Config
+var config = {
+  boardConfig : {
+    buttonPin : 2,
+    ledCount: 12
+  }
+}
+
+var board = require("./lib/arduino/board")(config);
+
+
+
+board.start().then(function(b){
+
+  var red = Color().rgb(255, 0, 0);
+  var green = Color().rgb(0, 255, 0);
+  var blue = Color().rgb(0, 0, 255);
+  var black = Color().rgb(0, 0, 0);
+
+  var pattern = [
+    [red],
+    [black],
+    [red, green],
+    [black],
+    [red, green, blue]
+    [black],
+    [blue]
+  ]
+
+  b.patternLeds(pattern, 500, 0);
+
+});
